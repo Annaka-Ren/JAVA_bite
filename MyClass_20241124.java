c5e9ec53ca626f0836fec9b882b4647932fcdbea
安中 蓮 <1584733077@qq.com>
2024-11-26 06:56:01 +0800

    Add files via upload

---
+++ MyClass_20241124.java
@@ -0,0 +1,19 @@
+public class MyClass_20241124 {
+    public static void main(String[] args) {
+//        int a = 0xa;//用16进制来表示2进制
+//        int b = 0xc;
+//        //a --> 1010
+//        //b --> 1100
+//        System.out.printf("0x%x\n", a & b);
+//        System.out.printf("0x%x\n", a | b);
+//        System.out.printf("0x%x\n", ~a);
+
+//        int a = 10;
+//        System.out.println(a << 3);//左移一位，相当于乘2
+//        System.out.println(a >> 1);//右移一位，相当于除以2。重点：整数除以整数
+        // >>>逻辑右移，空位补0
+//        int a = 10;
+//        int b = 20;
+//        System.out.println(a < b ? a : b);
+    }
+}
