6daf9d6d39c860d2b88ba3623d3a423ae6504b30
安中 蓮 <1584733077@qq.com>
2024-11-27 04:54:01 +0800

    Add files via upload

---
+++ MyClass_20241126.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class MyClass_20241126 {
+    public static void main(String[] args) {
+//        //使用while循环打印1 - 10
+//        int i = 1;
+//        while(i <= 10)
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
+        //计算1+2+3+...100的和
+//        int sum = 0;
+//        int i = 0;
+//        while(i <= 100){
+//            sum += i;
+//            i++;
+//        }
+////        System.out.println(sum);
+        //计算5的阶乘
+//        int result = 1;
+//        int i = 1;
+//        while(i <= 5){
+//            result *= i;
+//            i++;
+//        }
+//        System.out.println(result);
+        //输出 1000 - 2000 之间所有的闰年
+//        int year = 1000;
+//        while(year <= 2000){
+//            if((year % 4 == 0 && year % 100 != 0)||year % 400 == 0){
+//                System.out.println(year);
+//            }
+//            year++;
+//        }
+        //计算1！+2！+3！+4！+5！的值
+//        int sum = 0;
+//        int n = 1;
+//        while(n <= 5){
+//            int factorResult = 1;//表示阶乘的结果
+//            int i = 1;
+//            while (i <= n){
+//                factorResult *= i;
+//                i++;
+//            }
+//            //System.out.println(factorResult);
+//            sum += factorResult;
+//            n++;
+//        }
+//        System.out.println(sum);
+
+        //自主练习：找到100-200之间第一个3的倍数
+//        for(int i = 100; i <= 200; i++){
+//            if(i % 3 == 0){
+//                System.out.println(i);
+//                break;
+//            }
+//        }
+//        int i = 100;
+//        while(i <= 200){
+//            if(i % 3 != 0){
+//                i++;
+//                continue;
+//            }else{
+//                System.out.println(i);
+//                i++;
+//            }
+//        }
+        //打印1-10
+//        for(int a = 1; a <= 10; a++){
+//            System.out.println(a);
+////        }
+//        System.out.println("请输入一个整数：");
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//
+//        //针对特殊情况需要判定
+//        if(n == 0 || n == 1){
+//            System.out.println(n + "不是素数");
+//            return;
+//        }
+//        //从2开始，一直到n的平方根
+//        int i = 2;
+//        double tmp = Math.sqrt(n);
+//        for(; i < tmp; i++){
+//            if(n % 1 == 0){
+//                System.out.println(n +"不是素数");
+//                break;
+//            }
+//        }
+//        if(i >= tmp){
+//            System.out.println(n + "是素数");
+//        }
+    }
+}
