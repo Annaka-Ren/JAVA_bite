784baf989f3d10bbf9d9049ea6283b970f014127
安中 蓮 <1584733077@qq.com>
2024-11-29 04:59:40 +0800

    Add files via upload

---
+++ MyClass_20241128.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyClass_20241128 {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        if(scanner.hasNext()){
+//            String s = scanner.next();
+//            System.out.println("输入的字符串是：" + s);
+//        }else{
+//            System.out.println("输入有误");
+//        }
+        //处理多组输入，输入若干个整数，算平均值
+//        Scanner scanner = new Scanner(System.in);
+//        int sum = 0;//变量累加的结果
+//        int count = 0;//变量的个数
+//        while(scanner.hasNext()){
+//            int num = scanner.nextInt();
+//            System.out.println("输入的数字是：" + num);
+//            sum += num;
+//            count++;
+//        }
+//        System.out.println("总和是：" + sum);
+//        System.out.println("个数是：" + count);
+//        System.out.println("平均值是：" + (double)sum / count);
+        //猜数字
+        //1.先生成 1- 100之间的随机整数
+//        Random random = new Random();
+//        int answerNum = random.nextInt(100) + 1;
+//        //2.让用户输入一个数字，进行猜的过程
+//        Scanner scanner = new Scanner(System.in);
+//        while(true){
+//            System.out.println("请输入一个1-100范围内的整数：");
+//            int guessNum = scanner.nextInt();
+//            if(guessNum < answerNum){
+//                System.out.println("猜小了");
+//            }else if(guessNum > answerNum){
+//                System.out.println("猜大了");
+//            }else{
+//                System.out.println("猜对了");
+//                break;
+//            }
+//        }
+        //获取到系统的毫秒级时间戳
+//        System.out.println(System.currentTimeMillis());
+    }
+}
